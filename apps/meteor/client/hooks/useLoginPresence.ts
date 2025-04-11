0c1b39f2a8e530642749f08fff225a50204bf32a
juliajforesti <juliajforesti@gmail.com>
2025-04-11 14:04:08 -0300

    refactor: useLoginPresence

--- apps/meteor/client/hooks/useLoginPresence.ts
+++ apps/meteor/client/hooks/useLoginPresence.ts
@@ -1,6 +1,6 @@
-import { useConnectionStatus, useSetting, useUserId } from '@rocket.chat/ui-contexts';
-import { Meteor } from 'meteor/meteor';
-import { useCallback } from 'react';
+import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
+import { ServerContext, useConnectionStatus, useSetting, useUserId } from '@rocket.chat/ui-contexts';
+import { useContext } from 'react';
 
 import { useIdleDetection } from './useIdleDetection';
 
@@ -8,16 +8,21 @@ export const useLoginPresence = () => {
 	const uid = useUserId();
 	const { status } = useConnectionStatus();
 	const allowAnonymousRead = useSetting('Accounts_AllowAnonymousRead');
+	const { disconnect: disconnectServer, reconnect: reconnectServer } = useContext(ServerContext);
 
-	const disconnect = useCallback(() => {
+	const disconnect = useEffectEvent(() => {
 		if (status === 'offline') {
 			if (!uid && allowAnonymousRead !== true) {
-				Meteor.disconnect();
-				return;
+				disconnectServer();
 			}
-			Meteor.reconnect();
 		}
-	}, [allowAnonymousRead, status, uid]);
+	});
 
-	useIdleDetection(disconnect, { time: 3000 });
+	const reconnect = useEffectEvent(() => {
+		if (status === 'offline') {
+			reconnectServer();
+		}
+	});
+
+	useIdleDetection(disconnect, { onActive: reconnect });
 };
